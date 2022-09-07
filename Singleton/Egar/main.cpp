1bcc5aa39149373b955b8522e79b8fbf0935bc0d
asshukla <asshukla@adobe.com>
2022-09-07 22:04:07 +0530

    Why not to use reference read main.cpp

--- Singleton/Egar/main.cpp
+++ Singleton/Egar/main.cpp
@@ -10,9 +10,19 @@
 #include "Scene.hpp"
 
 int main() {
+    // If we keep refernece local to class it can get deleted and we might face issue of dangling refeence
     Logger& logger = Logger::GetInstance();
     logger.Log("=== Begin ===");
     Scene scene("newScene");
+    //
+    //    Scene scene1("scene1");
+    //    scene1 = scene; // this will not work
+     //   Scene scene2 = scene;// this will work
+    //    whenever we have reference variable as class member it's assignment operator will get deleted
+    // because we cannnot change what reference points to in assignment
+    // thats why we should not use reference
+    
+    
     scene.PrepareWorld();
     scene.LoadModels();
 
