903ab81fd729887689666726777f1312746784be
asshukla <asshukla@adobe.com>
2022-09-07 22:51:43 +0530

    Multi threaded example, multiple instance created and program has crashed

--- Singleton/Lazy/main.cpp
+++ Singleton/Lazy/main.cpp
@@ -9,29 +9,31 @@
 #include "Logger.hpp"
 #include "Scene.hpp"
 #include <iostream>
+#include <thread>
+
 int main() {
-    std::cout<<"Entering Main\n";
-    // If we keep refernece local to class it can get deleted and we might face issue of dangling refeence
-    //Logger& logger = Logger::GetInstance();
-    Logger::GetInstance().Log("=== Begin ===");
-    Scene scene("newScene");
-    //
-    //    Scene scene1("scene1");
-    //    scene1 = scene; // this will not work
-     //   Scene scene2 = scene;// this will work
-    //    whenever we have reference variable as class member it's assignment operator will get deleted
-    // because we cannnot change what reference points to in assignment
-    // thats why we should not use reference
-    
-    
-    scene.PrepareWorld();
-    scene.LoadModels();
-
-    /*
-     * Write your code here
-     */
-
-    Logger::GetInstance().Log("=== End ===");
+//    std::cout<<"Entering Main\n";
+//    // If we keep refernece local to class it can get deleted and we might face issue of dangling refeence
+//    //Logger& logger = Logger::GetInstance();
+//    Logger::GetInstance().Log("=== Begin ===");
+//    Scene scene("newScene");
+//    //
+//    //    Scene scene1("scene1");
+//    //    scene1 = scene; // this will not work
+//     //   Scene scene2 = scene;// this will work
+//    //    whenever we have reference variable as class member it's assignment operator will get deleted
+//    // because we cannnot change what reference points to in assignment
+//    // thats why we should not use reference
+//
+//
+//    scene.PrepareWorld();
+//    scene.LoadModels();
+//
+//    /*
+//     * Write your code here
+//     */
+//
+//    Logger::GetInstance().Log("=== End ===");
     // If we comment all above code still the logger instance gets created, This is called egar initilization
     /* std::cout<<"Entering Main\n";
     Logger()
@@ -44,4 +46,21 @@ int main() {
     // 2. memory leak => No memory leak , because once program exit all the memory will be released
     // this type of singleton is leaky singleton
     // what if the logger is relying on some system resource, so they will not get released, in that case we need somehow call destructor
+    
+    
+    // MutliThreaded application
+    
+    std::thread t1{[]() {
+        Logger& logger = Logger::GetInstance();
+        logger.Log("Thread 1 has started");
+    }};
+    
+    std::thread t2{[]() {
+        Logger& logger = Logger::GetInstance();
+        logger.Log("Thread 2 has started");
+    }};
+    
+    t1.join();
+    t2.join();
+    return 0;
 }
